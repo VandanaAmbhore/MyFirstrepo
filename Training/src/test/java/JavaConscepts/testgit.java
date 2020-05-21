ccfeb2ad4fb4067104bb4f722724cdc5393e0a36
VandanaAmbhore <vambhore99@gmail.com>
2020-05-21 12:52:29 +0530

    A class added

--- Training/src/test/java/JavaConscepts/testgit.java
+++ Training/src/test/java/JavaConscepts/testgit.java
@@ -5,7 +5,14 @@ public class testgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git project");
+		A a=new A();
+		a.i;
 
 	}
+	
 
+}
+class A{
+	
+	int i=10;
 }
